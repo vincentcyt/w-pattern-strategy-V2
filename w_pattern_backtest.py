27bd55241c586fcae4d955db1a1feb7a41da29e7
vincentcyt <chengyuan.uiuc@gmail.com>
2025-06-05 17:29:01 +0800

    Update w_pattern_backtest.py

--- w_pattern_backtest.py
+++ w_pattern_backtest.py
@@ -1,3 +1,3 @@
 import yfinance as yf
-df = yf.download("2330.TW", period="60d", interval="60m", auto_adjust=False)
+df = yf.download("2330.TW", period="60d", interval="10m", auto_adjust=False)
 print(df.head(), df.tail())
