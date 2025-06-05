14b939899b26dc577267fc7fbfb3e996c22f83a9
vincentcyt <chengyuan.uiuc@gmail.com>
2025-06-05 17:33:41 +0800

    Update w_pattern_backtest.py

--- w_pattern_backtest.py
+++ w_pattern_backtest.py
@@ -1,3 +1,11 @@
 import yfinance as yf
-df = yf.download("2330.TW", period="60d", interval="10m", auto_adjust=False)
-print(df.head(), df.tail())
+
+# 定义标的与数据区间
+TICKER = "2330.TW"
+PERIOD = "5d"   # 过去 5 天
+
+# 从 Yahoo Finance 下载数据
+df = yf.download(TICKER, period=PERIOD)
+
+# 显示结果
+print(df)
